c42c6d3972ba48c05c8056daadeaa3931c1c0f2d
Sohan Katragadda <katragaddasohan@gmail.com>
2024-01-09 19:20:30 +0530

    Final Commit+Push

--- Home.py
+++ Home.py
@@ -6,8 +6,23 @@ st.set_page_config(layout="wide")
 st.title("Home")
 ab1,ab2 = st.columns(2)
 about_me="""
-Hey! Im a 15 year old kid with a passion for photography and I wanted to share some of my work 
-Enjoy!
+My name is Sohan, I'm 15 years old and I recently picked up analogue photography.
+I never really considered myself to be the artistic kind as a kid.
+Clumsy with pencils and paintbrushes, I stopped exploring after I realised my limits were stuck to poorly made stick figures.
+However, during the pandemic, my restlessness led me to find my dad's old camera.
+
+For an entire year, I was deadset on repairing that camera, finding someone who still sells film, someone to develop that film and so on and so forth.
+Long story short, I ended up having to buy a new camera because I later found out that whatever issue it had, it was unrepairable.
+
+That brings us to my first roll of film that I shot back in June.
+I had never really used a camera of any kind other than the one on my phone and the lack of instant feedback is something which taught me to have foresight and be patient.
+I waited a month for the film to get developed and scanned, and I found that the anticipation and need to be precise in what I shoot (as it's not a cheap medium) paid back tenfold when I received my photos. 
+
+I was hooked.
+
+In the 6 months since I started, I have consumed loads and loads of information, from the technical aspects of film to even portraits by Raja Ravi Varma to open my mind on how I look at things.
+Even though it's an extremely cliched line, it did open up a whole new world for me- a way to show others what I saw, what I felt.
+This is something I definitely want to keep working on and have fun with
 """
 
 with ab1:
